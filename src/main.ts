1d28e74ea8be24c246e991cdec4a4ea6520e78f7
hellosooka <leo.zemtsoff@icloud.com>
2022-12-20 16:14:27 +0300

    add vercel.json

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('/api');
 
   const PORT = process.env.SERVER_PORT;
 
