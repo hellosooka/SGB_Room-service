a49fc2971b08e7c6bc1937aa65635fb6e86a3811
hellosooka <leo.zemtsoff@icloud.com>
2022-12-20 16:22:20 +0300

    delete /api

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('/api');
 
   const PORT = process.env.SERVER_PORT;
 
